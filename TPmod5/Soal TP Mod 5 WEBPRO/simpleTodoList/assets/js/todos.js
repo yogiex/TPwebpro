2d2435b3daaecd1d2e69cedfd6374acab22f1256
yogiex <priyagung.yoggi@gmail.com>
2019-09-28 22:47:44 +0700

    lupa commit minggu lalu

---
+++ TPmod5/Soal TP Mod 5 WEBPRO/simpleTodoList/assets/js/todos.js
@@ -0,0 +1,38 @@
+$('li').click(function() {
+  if ($(this).css('color') === 'rgb(128, 128, 128)') {
+    $(this).css({
+      color: 'black',
+      textDecoration: 'none'
+    });
+  } else {
+    $(this).css({
+      color: 'gray',
+      textDecoration: 'line-through'
+    });
+  }
+});
+$('#semangat').append(
+  "<audio controls autoplay hidden> <source src='../css/semangat.mp3' type='audio/mpeg' /></audio>"
+);
+// lengkapi jquery untuk menlist hasil dari todolist yang di inputkan
+$('ul').on('click', 'li', function() {
+  $(this).toogleClass("completed")
+});
+
+$("ul").on('click', "span", function (e) {
+  e.stopPropagation();
+  $(this).closest("li").fadeOut(500,function() {
+   $(this).remove();
+    updateNumbers();
+  });
+});
+
+$("input[type='text']").keypress(function(e) {
+  if(e.which === 13 || e.KeyCode == 13) {
+    var todoText = $(this).val();
+    if( $(this).val() !== "") {
+    $("ul").append("<li><span><i class='fa fa-trash'> </i></span>" + "<number></number>" + todoText + "</li>");
+      }
+    $(this).val("");
+  }
+});
